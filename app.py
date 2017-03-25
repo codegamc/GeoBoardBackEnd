ed948e973b7391241acf2cccc52b4978947c9371
Mike Codega <michael.c.codega@gmail.com>
2017-03-25 03:55:06 -0400

    Created api endpoints

--- app.py
+++ app.py
@@ -3,4 +3,21 @@
 Back end for GeoBoards project
 
 '''
+#imports
+from bottle import Bottle, request
 
+
+#########################################################
+api = Bottle()
+
+@api.get('/getposts')
+def get_posts():
+    #returns posts
+    print request
+
+@api.post('/newpost'):
+def new_post():
+    #saves new post to db
+    print request
+
+@
